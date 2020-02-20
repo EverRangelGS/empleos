230513ac2f8cd8378a47e24d5fca77dff5e0649f
Ever Rangel <everardo.flores@elektra.com.mx>
2020-02-20 17:31:31 -0600

    Actualizacion 20/02/20 ... 2

--- src/main/java/com/example/empleos/controller/VacantesController.java
+++ src/main/java/com/example/empleos/controller/VacantesController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.empleos.model.Vacante;
 import com.example.empleos.service.IVacantesService;
@@ -38,17 +41,25 @@ public class VacantesController {
 	}
 	
 	@GetMapping("/create")
-	public String crear() {
+	public String crear(Vacante vacante) {
 		return "vacantes/formVacante";
 	}
 	
 	//Con @RequestBody se realiza el DataBinding
 	@PostMapping("/save")
-	public String guardar(Vacante vacante, Model model) { 
+	public String guardar(Vacante vacante, BindingResult result, RedirectAttributes attributes) {
+		
+		if(result.hasErrors()) {
+			for(ObjectError error : result.getAllErrors()) {
+				System.out.println("Ocurrio un error: "+ error.getDefaultMessage());
+			}
+			return "vacantes/formVacante"; 
+		}
+		
 		serviceVacantes.guardar(vacante);
+		attributes.addFlashAttribute("msg", "Registro guardado"); //Desponible hasta que realizamos el redirect
 		System.out.println("Vacante: "+vacante);
-		//model.addAttribute("vacante", vacante);
-		return "vacantes/listVacantes";
+		return "redirect:/vacantes/index"; //implicitamente realizamos una segunda peticion HTTP
 	}
 	
 	@GetMapping("/delete")
